75502e25fe16002709d4b50c96d3baeb8ce5a555
kevinmost <kevin.most.93@gmail.com>
2013-11-03 02:26:46 -0500

    Java program up to subject selector
    Still have more to do, of course. Must make it select every subject and
    then iterate through every class, pulling relevant data.

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.List;
 
 import javax.swing.JOptionPane;
 
@@ -12,8 +13,10 @@ import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlAnchor;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlInput;
+import com.gargoylesoftware.htmlunit.html.HtmlOption;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
+import com.gargoylesoftware.htmlunit.html.HtmlSelect;
 import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
 
 
@@ -30,32 +33,35 @@ public class Test {
 		// TODO Auto-generated method stub
 		
 		final WebClient webClient = new WebClient(BrowserVersion.FIREFOX_17);
-		final HtmlPage loginPage = webClient.getPage("http://apollo.stjohns.edu");		
-		
-		
-		final String pageAsXml = loginPage.asXml();
-		final String pageAsText = loginPage.asText();
-		
-		// DO THINGS STARTING HERE
 		
 		// Get the login form, and then gets the submit button, user ID, and password fields from the form
+		final HtmlPage loginPage = webClient.getPage("http://apollo.stjohns.edu");				
 		final HtmlForm form = loginPage.getFormByName("loginform");
-		final HtmlSubmitInput submitbutton = form.getInputByValue("Login");
+		final HtmlSubmitInput submitButton = form.getInputByValue("Login");
 		final HtmlPasswordInput userid = form.getInputByName("sid");
 		final HtmlPasswordInput password = form.getInputByName("PIN");
-		
 		// Log into UIS
-		userid.setValueAttribute(JOptionPane.showInputDialog("Enter your X-number (including the \"X\")"));
-		password.setValueAttribute(JOptionPane.showInputDialog("Enter your password"));
+		userid.setValueAttribute("X02311516");
+		password.setValueAttribute("314159");
 		
-		// Submits the username and password, and gets the landing page that you are brought to (stored as "page1")
-		final HtmlPage page1 = submitbutton.click();
+		// Submits the username and password, and then brings you to the "look up classes" page
+		final HtmlPage page1 = submitButton.click();
+		final HtmlPage studentPage = page1.getAnchorByText("Student").click();
+		final HtmlPage registrationPage = studentPage.getAnchorByText("Registration").click();
+		final HtmlPage lookupClassesPage = registrationPage.getAnchorByText("Look-up Classes to Add").click();
 		
-		// As an example, we can now get the link that has the text "Financial Aid" and store it into the variable "financialAidLink"
-		final HtmlAnchor financialAidLink = page1.getAnchorByText("Financial Aid");
-		// We can then print out the URL that the link targets with .getHrefAttribute
-		System.out.println(financialAidLink.getHrefAttribute());
+		// Gets the term-selector form, its submit button, and the term selector box, sets the term to "Spring 2014", and submits
+		final HtmlForm termSelectorForm = lookupClassesPage.getForms().get(1);
+		final HtmlSubmitInput termSelectorSubmit = termSelectorForm.getInputByValue("Submit");
+		final HtmlSelect termSelector = termSelectorForm.getSelectByName("p_term");
+		termSelector.setSelectedAttribute(termSelector.getOptionByText("Spring 2014"), true);
+		final HtmlPage subjectsPage = termSelectorSubmit.click();
 		
+		// TODO: Select every element in the selection box (which has the name "sel_subj") and submit
+		final HtmlForm subjectSelectorForm = subjectsPage.getForms().get(1);
+		 
+		
+		System.out.println("hi");
 		webClient.closeAllWindows();
 	}
 
